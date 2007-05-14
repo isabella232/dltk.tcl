308952aa2d3138a99dc314a15ecf7a0c636bf270
asobolev <asobolev>
2007-05-14 06:34:29 +0000

    * headers. * 3.2.1 compatible code.

--- tcl/tests/org.eclipse.dltk.tcl.core.tests/rawtests/simple0.tcl
+++ tcl/tests/org.eclipse.dltk.tcl.core.tests/rawtests/simple0.tcl
@@ -1,3 +1,13 @@
+###############################################################################
+# Copyright (c) 2005, 2007 IBM Corporation and others.
+# All rights reserved. This program and the accompanying materials
+# are made available under the terms of the Eclipse Public License v1.0
+# which accompanies this distribution, and is available at
+# http://www.eclipse.org/legal/epl-v10.html
+#
+
+###############################################################################
+
 proc foo { a } {
 	puts $a ; puts "wow!"
 }
@@ -10,7 +20,7 @@ foo $x
 
 puts [expr {$x + 1}]
 
-set string "fuckingly! and number really is $x..."
+set string "_________! and number really is $x..."
 
 set {a
 b} 12 ; puts ${a

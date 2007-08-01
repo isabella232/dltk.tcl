16d45b8242845cec36451e7a06f4afa67ccadc37
asobolev <asobolev>
2007-08-01 09:56:51 +0000

    * revert to clear 0.9 version

--- tcl/plugins/org.eclipse.dltk.tcl.debug/src/org/eclipse/dltk/tcl/internal/debug/TclDebugConstants.java
+++
@@ -1,9 +0,0 @@
-package org.eclipse.dltk.tcl.internal.debug;
-
-public final class TclDebugConstants {
-	private TclDebugConstants () {
-		
-	}
-	
-	public static final String DEBUG_MODEL_ID = "org.eclipse.dltk.debug.tclModel";
-}

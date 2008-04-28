b59dd84d5c8123448bba822a48a2ed9194aafc86
asobolev <asobolev>
2008-04-28 08:39:20 +0000

    * merge with HEAD

--- tcl/plugins/org.eclipse.dltk.tcl.activestatedebugger/src/org/eclipse/dltk/tcl/activestatedebugger/TclActiveStateDebuggerRunner.java
+++ tcl/plugins/org.eclipse.dltk.tcl.activestatedebugger/src/org/eclipse/dltk/tcl/activestatedebugger/TclActiveStateDebuggerRunner.java
@@ -8,10 +8,13 @@
 
 package org.eclipse.dltk.tcl.activestatedebugger;
 
-import java.io.File;
 import java.util.List;
 
+import org.eclipse.core.runtime.Path;
 import org.eclipse.dltk.core.PreferencesLookupDelegate;
+import org.eclipse.dltk.core.environment.EnvironmentPathUtils;
+import org.eclipse.dltk.core.environment.IEnvironment;
+import org.eclipse.dltk.core.environment.IFileHandle;
 import org.eclipse.dltk.launching.ExternalDebuggingEngineRunner;
 import org.eclipse.dltk.launching.IInterpreterInstall;
 import org.eclipse.dltk.launching.InterpreterConfig;
@@ -31,8 +34,7 @@ import org.eclipse.dltk.utils.PlatformFileUtils;
 public class TclActiveStateDebuggerRunner extends ExternalDebuggingEngineRunner {
 	public static final String ENGINE_ID = "org.eclipse.dltk.tcl.activestatedebugger";
 
-	private static final String HOST_KEY = "-host-ide";
-	private static final String PORT_KEY = "-port-ide";
+	private static final String ADDRESS_KEY = "-dbgp";
 	private static final String SHELL_KEY = "-app-shell";
 	private static final String IDE_KEY = "-ide-key";
 	private static final String SCRIPT_KEY = "-app-file";
@@ -51,10 +53,9 @@ public class TclActiveStateDebuggerRunner extends ExternalDebuggingEngineRunner 
 	protected InterpreterConfig alterConfig(InterpreterConfig config,
 			PreferencesLookupDelegate delegate) {
 
-		File file = PlatformFileUtils
-				.findAbsoluteOrEclipseRelativeFile(getDebuggingEnginePath(delegate));
+		IFileHandle file = getDebuggingEnginePath(delegate);
 
-		final String exe = getInstall().getInstallLocation().getAbsolutePath();
+		final String exe = getInstall().getInstallLocation().toString();
 		final String host = (String) config
 				.getProperty(DbgpConstants.HOST_PROP);
 		final String port = (String) config
@@ -62,27 +63,32 @@ public class TclActiveStateDebuggerRunner extends ExternalDebuggingEngineRunner 
 		final String sessionId = (String) config
 				.getProperty(DbgpConstants.SESSION_ID_PROP);
 
-		File pdxFiles = PlatformFileUtils
-				.findAbsoluteOrEclipseRelativeFile(new File(
-						getDebuggingPreference(
-								delegate,
-								TclActiveStateDebuggerConstants.DEBUGGING_ENGINE_PDX_PATH_KEY)));
+		IEnvironment env = getInstall().getEnvironment();
+
+		String pathKeyValue = getDebuggingPreference(delegate,
+				TclActiveStateDebuggerConstants.DEBUGGING_ENGINE_PDX_PATH_KEY);
+
+		String path = (String) EnvironmentPathUtils.decodePaths(pathKeyValue)
+				.get(env);
 
 		InterpreterConfig newConfig = (InterpreterConfig) config.clone();
 
-		if (pdxFiles.exists()) {
-			newConfig.addEnvVar("TCLDEVKIT_LOCAL", pdxFiles.getAbsolutePath());
+		if (path != null) {
+			IFileHandle pdxFiles = PlatformFileUtils
+					.findAbsoluteOrEclipseRelativeFile(env, new Path(path));
+
+			if (pdxFiles.exists()) {
+				newConfig.addEnvVar("TCLDEVKIT_LOCAL", pdxFiles
+						.toOSString());
+			}
 		}
 
 		// Additional property
-		newConfig.setProperty(OVERRIDE_EXE, file.getAbsoluteFile().toString());
+		newConfig.setProperty(OVERRIDE_EXE, file.toString());
 
 		// Interpreter arguments
-		newConfig.addInterpreterArg(HOST_KEY);
-		newConfig.addInterpreterArg(host);
-
-		newConfig.addInterpreterArg(PORT_KEY);
-		newConfig.addInterpreterArg(port);
+		newConfig.addInterpreterArg(ADDRESS_KEY);
+		newConfig.addInterpreterArg(host + ':' + port);
 
 		newConfig.addInterpreterArg(SHELL_KEY);
 		newConfig.addInterpreterArg(exe);
@@ -90,12 +96,12 @@ public class TclActiveStateDebuggerRunner extends ExternalDebuggingEngineRunner 
 		newConfig.addInterpreterArg(IDE_KEY);
 		newConfig.addInterpreterArg(sessionId);
 
-		if (isLoggingEnabled(delegate)) {
+
+		String logFileName = getLogFileName(delegate, sessionId);
+		if (logFileName != null) {
 			newConfig.addInterpreterArg(LOG_KEY);
 			newConfig.addInterpreterArg(LOG_FILE_KEY);
-
-			File logFileName = getLogFileName(delegate, sessionId);
-			newConfig.addInterpreterArg(logFileName.getAbsolutePath());
+			newConfig.addInterpreterArg(logFileName);
 		}
 
 		newConfig.addInterpreterArg(SCRIPT_KEY);

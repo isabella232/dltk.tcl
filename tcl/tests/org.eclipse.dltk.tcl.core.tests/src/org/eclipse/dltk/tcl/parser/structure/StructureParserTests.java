a9fc6c564c6c670ab6c2fa4a7da42ae3d3a28e2e
Mat Booth <mat.booth@redhat.com>
2016-04-08 14:50:34 +0100

    Bug 491397 - Test failure when executed in Eclipse from binary test bundle
    It caused by the test mistakenly using the maven metadata inside
    META-INF as a Tcl test resource. It is fixed by explicitly excluding
    anything whose path begins with "/META-INF".
    
    Change-Id: I45673c39d7d77a4b8e1a2d6f98c844cb0d3aea72
    Signed-off-by: Mat Booth <mat.booth@redhat.com>

--- tcl/tests/org.eclipse.dltk.tcl.core.tests/src/org/eclipse/dltk/tcl/parser/structure/StructureParserTests.java
+++ tcl/tests/org.eclipse.dltk.tcl.core.tests/src/org/eclipse/dltk/tcl/parser/structure/StructureParserTests.java
@@ -1,5 +1,5 @@
 /*******************************************************************************
- * Copyright (c) 2009 xored software, Inc.  
+ * Copyright (c) 2009, 2016 xored software, Inc and others.
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
@@ -139,7 +139,10 @@ public class StructureParserTests extends TestCase {
 		@SuppressWarnings("unchecked")
 		Enumeration<URL> e = bundle.findEntries(path, pattern, true);
 		while (e.hasMoreElements()) {
-			suite.addTest(new StructureParserTests(e.nextElement()));
+			URL url = e.nextElement();
+			if (!url.getPath().startsWith("/META-INF")) {
+				suite.addTest(new StructureParserTests(url));
+			}
 		}
 		return suite;
 	}

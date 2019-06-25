203cce65bca35d567db5a7aa512a5b0ecae7cef7
Alexander Kurtakov <akurtako@redhat.com>
2019-06-25 10:55:30 +0300

    Remove deprecated code in internal packages.
    Change-Id: Ie0232c13f41a5681fe252e4e2c1ae910997c706e
    Signed-off-by: Alexander Kurtakov <akurtako@redhat.com>

--- tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/editor/TclEditor.java
+++ tcl/plugins/org.eclipse.dltk.tcl.ui/src/org/eclipse/dltk/tcl/internal/ui/editor/TclEditor.java
@@ -3,7 +3,7 @@
  * This program and the accompanying materials are made available under the
  * terms of the Eclipse Public License v. 2.0 which is available at
  * http://www.eclipse.org/legal/epl-2.0.
- * 
+ *
  * SPDX-License-Identifier: EPL-2.0
  *
  *******************************************************************************/
@@ -99,7 +99,7 @@ public class TclEditor extends ScriptEditor {
 	}
 
 	@Override
-	protected IFoldingStructureProvider getFoldingStructureProvider() {
+	protected IFoldingStructureProvider createFoldingStructureProvider() {
 		if (foldingProvider == null) {
 			foldingProvider = new TclFoldingStructureProvider();
 		}
